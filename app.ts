fe0ab4e6732731eed52da42546850cf8217c43d9
David Gevorgyan <greencross.am@gmail.com>
2020-06-08 18:56:20 +0400

    [WIP] more data types

--- app.ts
+++ app.ts
@@ -1,17 +1,5 @@
-const combine = (
-  input1: (number | string),
-  input2: (number | string),
-  resultConversion: string
-) => {
-  let result;
+let userInput: unknown;
 
-  if (typeof input1 === 'number' && typeof input2 === 'number') {
-    result = input1 + input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-
-  return result;
-}
-
-let test = 3.2;
+userInput = {};
+userInput = [];
+userInput = null;
---
+++ union-aliases.ts
@@ -0,0 +1,20 @@
+type Combinable = number | string;
+type ConversionType = 'as-number' | 'as-text';
+
+const combine = (
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionType
+) => {
+  let result;
+
+  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+
+  return result;
+}
+
+let test = 3.2;
